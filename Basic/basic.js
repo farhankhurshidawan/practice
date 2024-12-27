40eeaac87aac778662ce7b1023cdc7978c0e1424
Farhan Khurshid awan <mfarhanprofessional@gmail.com>
2024-12-28 01:40:39 +0500

    add data type

---
+++ Basic/basic.js
@@ -0,0 +1,25 @@
+// js Output => innerHTML,document.write,window.alert,window.print,console.log
+
+   
+
+    // ******************** variable *******************************
+    // let and const have block scope.
+    // let and const can not be redeclared.
+    // let and const must be declared before use.
+    // let and const does not bind to this.
+    // let and const are not hoisted.
+    // var does not have to be declared.
+    // var is hoisted.
+    // var binds to this.
+
+    //******************** Operator ****************************
+    // Arthematic operator : +,-,*,/,**,%,++,--
+    // Assignement operator: =,+=,-+,*=,/=,%=,**=
+    // comparison Operator : ==,===,!=,!==,>,<,<=,>=,?(ternary Operator)
+    // Ternary operator    : condition ? true:false
+    // Logical Operator    : &&(multiple true=true) || (just one true=true) !(opposite)
+   
+    // *************************Data Type*********************************
+    // let a = null;   // explicitly set to null no memory address
+    // let b;          // declared but not assigned, so it's undefined no memory address
+    
