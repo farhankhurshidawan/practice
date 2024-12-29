273308d35953e8dcb8bc6466c8550dc5f7b0b58a
Farhan Khurshid Awan <mfarhanprofessional@gmail.com>
2024-12-29 17:01:39 -0500

    Object are containers for properties and Method.

--- object/obj.js
+++ object/obj.js
@@ -1,4 +1,24 @@
-// How to Define a JavaScript Object
-// 1) Using an Object Literal
-// 2) Using the new Keyword
+// Objects are containers for Properties and Methods.
+// Properties key(name) and values("farhan")
+// methods are function stored as properties
+
+// ********************How to Define a JavaScript Object ****************************
+// // 1) Using an Object Literal
+// // An object literal is a list of name:value pairs inside curly braces {}.
+// const method1={
+//     Brother2:"Burhan",
+//     MyName:"Farhan"
+// }
+// const method2={}
+// method2.Brother3="reehan"
+// method2.Brother4="hamdan"
+// console.log(`${method1.MyName} and ${method2.Brother3}`);
+
+// // 2) Using the new Keyword
+// const newKeyword=new Object()
+// newKeyword.name="Farhan"
+// newKeyword.age="Burhan"
+// console.log(newKeyword);
+
 // 3) Using an Object Constructor
+
